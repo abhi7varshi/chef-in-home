9e9a3c0239ddedd61813ab4b1dac92facc28cbec
Abhishek M <abhivarshi123@gmail.com>
2024-12-12 07:02:19 +0530

    init

---
+++ DeliveryApp/app/src/main/java/com/example/deliveryapp/MainActivity.kt
@@ -0,0 +1,59 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.deliveryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.deliveryapp.ui.theme.DeliveryAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DeliveryAppTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen() {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Home Screen") })
+    }) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Hello, World!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DeliveryAppTheme {
+        HomeScreen()
+    }
+}
---
+++ UserApp/app/src/main/java/com/example/userapp/MainActivity.kt
@@ -0,0 +1,59 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.userapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.userapp.ui.theme.UserAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UserAppTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen() {
+    Scaffold(topBar = {
+        TopAppBar(title = { Text("Home Screen") })
+    }) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Hello, World!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UserAppTheme {
+        HomeScreen()
+    }
+}
