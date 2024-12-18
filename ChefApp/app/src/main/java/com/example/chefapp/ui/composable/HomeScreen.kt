b8c72fc840fb80db4d11e167cfc7bf86c1f63fba
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2024-12-18 22:25:20 +0530

    Added Dummy Data! and displaying it in Menu Content.

--- ChefApp/app/src/main/java/com/example/chefapp/ui/composable/HomeScreen.kt
+++ ChefApp/app/src/main/java/com/example/chefapp/ui/composable/HomeScreen.kt
@@ -1,17 +1,14 @@
 package com.example.chefapp.ui.composable
 
 import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
@@ -20,14 +17,12 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +30,7 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.chefapp.SettingsRepository
 import com.example.chefapp.SettingsViewModel
+import com.example.chefapp.data.dummyOrders
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -128,7 +124,7 @@ fun HomeScreen(
         ) {
             when (selectedIndex.intValue) {
                 0 -> HomeContent(isDarkModeEnabled = isDarkModeEnabled.value)
-                1 -> Text("Menu Content")
+                1 -> MenuContent(orders = dummyOrders)
                 2 -> Text("Settings Content")
             }
         }
